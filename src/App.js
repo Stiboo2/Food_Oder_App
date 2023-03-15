2f7ff84ee0cf54385b38fa6a5a114c53d634181b
Thabo <thabo.radebe@live.com>
2023-03-15 21:29:24 +0200

    40. Managing Cart & Modal State

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
-import Cart from './components/Cart/Cart'
+import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [cartIsShown, setCartIsShow] = useState(false);
+  const showCardHandler = () => {
+    setCartIsShow(true);
+  };
+  const hideCardHandler = () => {
+    setCartIsShow(false);
+  };
   return (
     <Fragment>
-      <Cart />
-      <Header/>
+      {cartIsShown && <Cart  onCloseCart={hideCardHandler}/>}
+      <Header onShowCart={showCardHandler} />
       <main>
         <Meals></Meals>
       </main>
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,14 @@
-import classes from './HeaderCardButton.module.css'
-import CartIcon from '../Cart/CartIcon';
-const HeaderCartButton=(props)=> {
-return (
-    <button className={classes.button}>
-<span className={classes.icon}>
-<CartIcon />
-</span>
-<span>
-    Your Cart
-</span>
-<span className={classes.badge}>3</span>
+import classes from "./HeaderCardButton.module.css";
+import CartIcon from "../Cart/CartIcon";
+const HeaderCartButton = (props) => {
+  return (
+    <button className={classes.button} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>3</span>
     </button>
-);
+  );
 };
-export default HeaderCartButton; 
+export default HeaderCartButton;
